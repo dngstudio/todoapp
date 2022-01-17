bf932d5d65e40bc8bfd9858b2cfe4311536d24c0
DNG Studio <office@dngstudio.co>
2022-01-17 19:21:23 +0100

    Fixes

---
+++ comps/Todo.js
@@ -0,0 +1,60 @@
+
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { Box } from '@mui/system';
+
+
+const Todo = (props) => {
+
+const [hovered, setHovered] = useState(false)
+
+const onEnter = () => {
+    setHovered(prevHovered => !prevHovered)
+}
+
+const onLeave = () => {
+    setHovered(prevHovered => !prevHovered)
+}
+
+const handleClick = () => {
+    setHovered(prevHovered => !prevHovered)
+}
+
+
+const style = {
+    textDecoration: hovered ? "line-through" : "none",
+    cursor:"pointer"
+}
+
+const trashStyle = {
+    opacity: hovered ? "1" : "0",
+    width: "20px",
+    position: "relative",
+    
+}
+
+    return ( 
+        <Box  sx={{ mt:1, display: 'flex', justifyContent: 'space-between'}}>
+            <Typography
+            style={style}
+            onClick = {handleClick}
+            onMouseEnter = {onEnter}
+            onMouseLeave = {onLeave}
+            variant="p"
+            color="primary"
+           
+            >
+            {props.todoText}
+            </Typography>
+
+            <DeleteOutlineOutlinedIcon
+                color='warning'
+                style={trashStyle}
+                onClick={props.handleDelete}
+            />
+        </Box>
+     );
+}
+ 
+export default Todo;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const users = [{ id: 1 }, { id: 2 }, { id: 3 }]
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(users)
 }
